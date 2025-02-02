5b3973aa38b1a597257f7ee81459507090848828
saqib-21 <saqibmominkhan@gmail.com>
2025-02-01 23:55:58 -0500

    -p allows for factorized path

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/PathValidator.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathValidator.java
@@ -10,24 +10,44 @@ public class PathValidator extends MazeRunner {
         this.maze = maze;
     }
 
+    
     public boolean isValidPath(String path) {
         char[][] grid = maze.getGrid();
-
-        for (char move : path.toCharArray()) {
-            if (move == 'F') {
-                moveForward(grid);
-            } else if (move == 'L') {
-                turnLeft();
-            } else if (move == 'R') {
-                turnRight();
-            } else {
-                return false; // Invalid character in path
+        int i = 0;
+    
+        while (i < path.length()) {
+            int repeat = 1; // Default to 1 if no number is provided
+    
+            // Check if there's a number before the move character
+            if (Character.isDigit(path.charAt(i))) {
+                repeat = 0;
+                while (i < path.length() && Character.isDigit(path.charAt(i))) {
+                    repeat = repeat * 10 + (path.charAt(i) - '0');
+                    i++;
+                }
+            }
+    
+            if (i < path.length()) {
+                char move = path.charAt(i);
+    
+                if (move == 'F') {
+                    for (int j = 0; j < repeat; j++) moveForward(grid);
+                } else if (move == 'L') {
+                    for (int j = 0; j < repeat; j++) turnLeft();
+                } else if (move == 'R') {
+                    for (int j = 0; j < repeat; j++) turnRight();
+                } else {
+                    return false; // Invalid move character
+                }
+    
+                i++; // Move to next character
             }
         }
-
+    
         return isAtExit();
     }
-
+    
+    
     private boolean isAtExit() {
         int[] exit = maze.findExit();
         return getPositionX() == exit[0] && getPositionY() == exit[1];
